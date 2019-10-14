d645864758170f65e4c2f7fc87a7fc2bac8722a8
Joko Priyono <joe.mandrake007@gmail.com>
2019-10-14 22:56:26 +0700

    Adding assets again

--- app/src/main/java/com/jo/belajarjetpackpro/ui/home/HomeActivity.kt
+++ app/src/main/java/com/jo/belajarjetpackpro/ui/home/HomeActivity.kt
@@ -1,13 +1,30 @@
 package com.jo.belajarjetpackpro.ui.home
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.jo.belajarjetpackpro.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+
+    fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+                txt.setText(R.string.home)
+                return true
+            }
+            R.id.action_bookmark -> {
+                txt.setText(R.string.bookmark)
+                return true
+            }
+        }
+        return false
     }
 }
